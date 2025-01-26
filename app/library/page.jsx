4a668c2a0fdaed959b7471f762b656b5f8c77c3d
4nglp <4nglpw@gmail.com>
2025-01-26 18:49:48 +0100

    updated the library

--- app/library/page.jsx
+++ app/library/page.jsx
@@ -1,17 +1,22 @@
 "use client";
+
 import Nav from "@components/Nav";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import Image from "@node_modules/next/image";
+
+const RenderSearchParams = () => {
+  const searchParams = useSearchParams();
+  return searchParams.get("tab") || "Reading";
+};
 
 const LibraryPage = () => {
   const [library, setLibrary] = useState([]);
   const [menuVisible, setMenuVisible] = useState(false);
   const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
   const [selectedMangaId, setSelectedMangaId] = useState(null);
-
-  const searchParams = useSearchParams();
-  const selectedCategory = searchParams.get("tab") || "Reading"; // Default to 'Reading'
+  const [selectedCategory, setSelectedCategory] = useState("Reading");
 
   useEffect(() => {
     const storedLibrary = JSON.parse(localStorage.getItem("library")) || [];
@@ -47,6 +52,13 @@ const LibraryPage = () => {
     };
   }, [menuVisible]);
 
+  // Update selected category when params change
+  useEffect(() => {
+    const storedCategory =
+      localStorage.getItem("selectedCategory") || "Reading";
+    setSelectedCategory(storedCategory);
+  }, []);
+
   // Filter library based on selected category
   const filteredLibrary = library.filter(
     (manga) => manga.category === selectedCategory
@@ -55,7 +67,6 @@ const LibraryPage = () => {
   return (
     <>
       <Nav />
-
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-6">Library</h1>
         <div className="mb-4 flex gap-4">
@@ -74,11 +85,14 @@ const LibraryPage = () => {
           </Link>
         </div>
 
+        <Suspense fallback={<p>Loading...</p>}>
+          <RenderSearchParams />
+        </Suspense>
+
         {filteredLibrary.length === 0 ? (
-          <h2>Category is empty.</h2>
+          <h2>Your library is empty for this category.</h2>
         ) : (
           <div className="grid grid-cols-5 gap-4">
-            {/* Added gap-4 here */}
             {filteredLibrary.map((manga) => (
               <div
                 key={manga.id}
@@ -87,10 +101,12 @@ const LibraryPage = () => {
               >
                 <Link href={`/manga/${manga.id}`}>
                   <div className="relative w-48 h-72 bg-gray-200 overflow-hidden">
-                    <img
-                      src={manga.coverImageUrl + ".256.jpg"}
+                    <Image
+                      src={manga.coverImageUrl}
                       alt={manga.title}
                       className="object-cover w-full h-full"
+                      width={400}
+                      height={800}
                     />
                     <div className="absolute bottom-0 w-full bg-black bg-opacity-60 text-white text-center py-2">
                       {manga.title}
