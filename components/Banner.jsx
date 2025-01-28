7f22ed4cb846beeac166be3db82807e767a5f803
4nglp <4nglpw@gmail.com>
2025-01-28 19:07:07 +0100

    added mobile responsivity for the banner on the home page

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 const Banner = ({ mangaList }) => {
   return (
     <div className="w-full">
-      <h1 className="text-2xl font-bold mb-4">4nglp&apos;s recommendations </h1>
+      <h1 className="text-2xl font-bold mb-4">4nglp&apos;s recommendations</h1>
       <Swiper
         modules={[Pagination, Autoplay]}
         slidesPerView={1}
@@ -31,28 +31,30 @@ const Banner = ({ mangaList }) => {
                   layout="fill"
                   objectFit="cover"
                   objectPosition="top"
-                  className=" opacity-80"
+                  className="opacity-80"
                 />
                 <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-between p-4 bg-gradient-to-r from-black to-transparent">
-                  <div className="flex-shrink-0 w-[200px] h-[300px]">
+                  <div className="flex-shrink-0 w-[150px] h-[225px] sm:w-[200px] sm:h-[300px]">
                     <Image
                       src={manga.coverUrl}
                       alt={manga.title}
-                      width={200}
-                      height={300}
+                      width={150}
+                      height={225}
                       objectFit="cover"
-                      className="rounded-md"
+                      className="rounded-md sm:w-[200px] sm:h-[300px]"
                     />
                   </div>
                   <div className="flex-1 ml-4 text-white">
-                    <h1 className="text-5xl font-bold">{manga.title}</h1>
-                    <h3 className="text-10sm mt-2">
+                    <h1 className="text-3xl sm:text-5xl font-bold">
+                      {manga.title}
+                    </h1>
+                    <h3 className="hidden sm:block text-sm mt-2">
                       {manga.genres || "No genres available"}
                     </h3>
-                    <div className="text-sm mt-2 max-h-[150px] overflow-y-auto">
+                    <div className="hidden sm:block text-sm mt-2 max-h-[150px] overflow-y-auto">
                       <p>{manga.description || "No description available"}</p>
                     </div>
-                    <h2 className="text-2xl italic text-bold mt-4">
+                    <h2 className="text-lg sm:text-2xl italic font-bold mt-4">
                       {manga.author || "No additional details available"}
                     </h2>
                   </div>
